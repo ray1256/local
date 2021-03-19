048cf30b467be0c2c8e8c311008386b7a7f81442
lucyhoho <kuowei@LucyHohos-Air.local>
2021-03-19 16:45:34 +0800

    four

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -128,3 +128,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
